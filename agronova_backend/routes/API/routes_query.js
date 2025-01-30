d28053e25df1a315bf826c0f92f1ee3ac3bad6e6
jordantek <jordan.da-silva-domingo@epitech.eu>
2025-01-30 11:44:20 +0100

    API URL construction

--- agronova_backend/routes/API/routes_query.js
+++ agronova_backend/routes/API/routes_query.js
@@ -1,8 +1,7 @@
-require('dotenv').config()
 const express = require('express');
-const data = require('./service');
+const { generateToken } = require('./service');
+
 const user = express.Router();
 
-user.get('/access-token', data.generete_token);
 
 module.exports = user;
