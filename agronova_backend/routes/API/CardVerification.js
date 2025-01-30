5b0c04f81829ccb00caee80b171d7373644a44cc
jordantek <jordan.da-silva-domingo@epitech.eu>
2025-01-31 00:27:38 +0100

    Update Card Verification

--- agronova_backend/routes/API/CardVerification.js
+++ agronova_backend/routes/API/CardVerification.js
@@ -1,8 +1,10 @@
-const { generateToken } = require('./services');
 const axios = require('axios');
+const { generateToken } = require('./service');
 
 async function DoAccountNameInquiry(accountDetails) {
     const accessToken = await generateToken();
+    if (!accessToken) throw new Error("Impossible d'obtenir un access token.");
+
     const terminalId = '3PBL0001';
     const bankCode = accountDetails.bankCode;  
     const accountId = accountDetails.accountId;
@@ -21,21 +23,22 @@ async function DoAccountNameInquiry(accountDetails) {
         const response = await axios.get(url, { headers });
         return response.data;
     } catch (error) {
-        throw new Error(error.response ? error.response.data : error.message);
+        throw new Error(error.response?.data || error.message);
     }
 }
 
 async function CheckCard(req, res) {
     try {
         const cardDetails = req.body;
+        const accessToken = await generateToken();
+        console.log("TOKEN:", accessToken);
+        
         const result = await DoAccountNameInquiry(cardDetails);
         res.status(200).json(result);
     } catch (error) {
-        const errorMessage = error.response ? error.response.data : error.message;
-        res.status(400).json({ error: errorMessage });
+        res.status(400).json({ error: error.message });
     }
 }
 
-module.exports = {
-    CheckCard
-};
+
+module.exports = { CheckCard };
