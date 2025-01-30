a4c33802e702c8a05178fc5931a86edbfa2015f1
jordantek <jordan.da-silva-domingo@epitech.eu>
2025-01-31 00:26:58 +0100

    Organised middleware

--- agronova_backend/index.js
+++ agronova_backend/index.js
@@ -1,31 +1,27 @@
-require('dotenv').config()
-const fs = require('fs');
-const {checkEmail, checkPassword, checkEmailExists, LoginUser, authToken} = require('./middleware/auth');
-const db = require('./config/db');
+require('dotenv').config();
 const cors = require('cors');
 const express = require('express');
 const app = express();
-const port = process.env.PORT;
-const body = require('body-parser');
-const mysql = require('mysql2');
-const Roads = require('./routes/auth/auth');
-const {generete_token} = require('./routes/API/service');
-const user = require('./routes/API/routes_query');
+const port = process.env.PORT || 3000; // Ajout d'un port par défaut
+const userRoutes = require('./routes/API/routes_query'); 
+const { generateToken } = require('./routes/API/service');
 
 app.use(cors());
-
-app.use(body.urlencoded({extended: false}));
-app.use(body.json());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.post('/info', (req, res) => {
     res.send("Hello World");
     console.log("Hello guys, I need some chocolate");
 });
 
-// app.use(api);
-app.use(user);
-// generete_token
+app.use('/api', userRoutes);
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({ error: 'Une erreur est survenue !' });
+});
 
 app.listen(port, () => {
-    console.log('Agronova app successfully listening at http://localhost:' + port);
+    console.log(`Agronova app successfully listening at http://localhost:${port}`);
 });
