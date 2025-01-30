bf1bcea61c3eb4023c7f7ccc8285db003eee6ebd
cynthia.zinsou <cynthia.zinsou@epitech.eu>
2025-01-30 20:07:52 +0100

    add: App.vue file

--- FRONT/src/App.vue
+++ FRONT/src/App.vue
@@ -1,40 +1,16 @@
 <template>
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap">
-  <!-- <img  class="imgf" alt="Vue logo" src="./assets/th.png" width="125" height="120">
-  <h1 class="soul">oul connexion</h1> -->
-  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-  <!-- <LoginView/> -->
-  <RouterView /> 
-  <!-- <Login/> -->
-  
+  <div>
+    <NavbarView />
+    <router-view></router-view>
+  </div>
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
-import Login from './components/Login.vue'
+import NavbarView from './components/NavbarView.vue';
+
 export default {
-  name: 'App',
   components: {
-    // HelloWorld,
-    // LoginView
-  }
-}
-
-
+    NavbarView,
+  },
+};
 </script>
-<style>
-.imgf{
-  position: relative;
-  left:460px;
-  top:-10px;
-}
-
-.soul {
-  position:relative;
-  left:550px;
-  top:-82px;
-  font-size: 2em;
-  font-family: 'Roboto', sans-serif;
-
-}
-</style>
