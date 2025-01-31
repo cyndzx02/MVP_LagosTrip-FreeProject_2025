5c83a6cefdda20e2f0cb0e42164fb4b970527638
jordantek <jordan.da-silva-domingo@epitech.eu>
2025-01-31 02:01:16 +0100

    FUnd transfer function completion

--- agronova_backend/routes/API/fundTransfer.js
+++ agronova_backend/routes/API/fundTransfer.js
@@ -7,6 +7,35 @@ function generateTransferCode() {
     return `${timestamp}${randomPart}`;
 }
 
+async function fundTransfer(transferDetails) {
+    try {
+        const token = await generateToken();
+        const url = "https://qa.interswitchng.com/quicktellerservice/api/v5/transactions/TransferFunds";
+
+        const headers = {
+            "Authorization": `Bearer ${token}`,
+            "TerminalID": "3PBL",
+            "Content-Type": "application/json"
+        };
+
+        const body = {
+            transferCode: generateTransferCode(),
+            mac: transferDetails.mac,
+            termination: transferDetails.termination,
+            sender: transferDetails.sender,
+            initiatingEntityCode: transferDetails.initiatingEntityCode,
+            initiation: transferDetails.initiation,
+            beneficiary: transferDetails.beneficiary
+        };
+
+        const response = await axios.post(url, body, { headers });
+        return response.data;
+    } catch (error) {
+        console.error("Erreur lors du transfert :", error.response ? error.response.data : error.message);
+        throw new Error(error.response ? JSON.stringify(error.response.data) : error.message);
+    }
+}
+
 module.exports = {
     fundTransfer
 };
